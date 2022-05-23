e87860e40a2e570bc7db7a35d80728bd99c37e55
leUnknown64 <dergermankind67@gmail.com>
2022-05-22 22:41:21 -0400

    texture support in main

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
+#include<filesystem>
+namespace fs = std::filesystem;
+
 #include<iostream>
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
+#include<stb/stb_image.h>
 #include "shaderClass.h"
 #include "EBO.h"
 #include "VAO.h"
 #include "VBO.h"
+#include "Texture.h"
 
 /*
  * Based off of OpenGL Crash Course Tutorial on YouTube
@@ -12,21 +17,19 @@
 
 // Vertices for equilateral triangle
 GLfloat vertices[] =
-{
-	-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // lower left corner
-	0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // lower right corner
-	0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f, // upper corner
-	-0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // inner left
-	0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // inner right
-	0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f // inner down
+{ //          COORDINATES     /    COLORS         /   TEXCOORD  //
+	-0.5f, -0.5f, 0.0f,         1.0f, 0.0f, 0.0f,   0.0f, 0.0f, // lower left corner
+	-0.5f,  0.5f, 0.0f,         0.0f, 1.0f, 0.0f,   0.0f, 1.0f, // upper left corner
+	 0.5f,  0.5f, 0.0f,         0.0f, 0.0f, 1.0f,   1.0f, 1.0f, // upper right corner
+	 0.5f, -0.5f, 0.0f,         1.0f, 1.0f, 1.0f,   1.0f, 0.0f  // lower right corner
 };
 
 // Indices for vertices order
 GLuint indices[] =
+// equilateral triangle
 {
-	0, 3, 5, // lower left triangle
-	3, 2, 4, // lower right triangle
-	5, 4, 1  // upper triangle
+	0, 2, 1,
+	0, 3, 2
 };
 
 int main(){
@@ -40,7 +43,7 @@ int main(){
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	// Create window (width, height, title, fullscreen, other)
-	GLFWwindow* window = glfwCreateWindow(800, 800, "My Triangle please enjoy :)", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(800, 800, "Texture Time", NULL, NULL);
 	if (window == NULL){
 		// verify window opens correctly
 		std::cout << "failed to create glfw window" << std::endl;
@@ -68,12 +71,25 @@ int main(){
 	EBO EBO1(indices, sizeof(indices));
 
 	// Link VBO to VAO
-	VAO1.linkVBO(VBO1, 0);
+	VAO1.linkAttrib(VBO1, 0, 3, GL_FLOAT, 8 * sizeof(float), (void*)0);
+	VAO1.linkAttrib(VBO1, 1, 3, GL_FLOAT, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	VAO1.linkAttrib(VBO1, 2, 2, GL_FLOAT, 8 * sizeof(float), (void*)(6 * sizeof(float)));
 	// Unbind all to prevent modification
 	VAO1.Unbind();
 	VBO1.Unbind();
 	EBO1.Unbind();
 
+	// Gets ID of uniform called scale in default.vert
+	GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
+
+	// Path for parent directory
+	std::string parentDir = fs::current_path().string();
+	// Path for texture files
+	std::string texPath = "/Resources/Textures/";
+
+	// Load in an image as a texture
+	Texture monkaS((parentDir + texPath + "monkaS.png").c_str(), GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
+	monkaS.texUnit(shaderProgram, "tex0", 0);
 
 	// Main loop
 
@@ -85,11 +101,15 @@ int main(){
 		
 		// Activate our shader program for OpenGl
 		shaderProgram.Activate();
+		// Set uniID's value (always set values after the shader program is activated!!!)
+		glUniform1f(uniID, 0.5f);
+		// Bind texture
+		monkaS.Bind();
 		// Bind VAO so OpenGl uses it
 		VAO1.Bind();
 		
 		// Draw primitives, number of indices, datatype of indices, index of indices
-		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		glfwSwapBuffers(window);
 
 		// Poll all GLFW events
@@ -100,6 +120,7 @@ int main(){
 	VAO1.Delete();
 	VBO1.Delete();
 	EBO1.Delete();
+	monkaS.Delete();
 	shaderProgram.Delete();
 
 	glfwDestroyWindow(window);
